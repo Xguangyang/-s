fffb664a9049fc517b6841ec21e537ec2b53303b
NaNa1839 <2692524400@qq.com>
2021-09-02 14:20:19 +0800

    9.392

--- TMS.API/Controllers/RedisController.cs
+++ TMS.API/Controllers/RedisController.cs
@@ -41,98 +41,98 @@ namespace TMS.API.Controllers
 
 
 
-        //学生类
-        public class Student
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public string Pwd { get; set; }
-        }
+        ////学生类
+        //public class Student
+        //{
+        //    public int Id { get; set; }
+        //    public string Name { get; set; }
+        //    public string Pwd { get; set; }
+        //}
 
 
 
 
-        //redis key
-        int REDISKEY = 1;
-        //得到Redis缓存数据
-        [HttpGet]
-        public string GetRedis(EnumType.RedisType redisType)
-        {
-            //集合模拟数据
-            List<Student> students = new List<Student>()
-            {
-                new Student { Id = 1, Name = "王", Pwd = "111" },
-                new Student { Id = 2, Name = "王12", Pwd = "222" },
-                new Student { Id = 3, Name = "王23", Pwd = "333" }
-            };
-            //模拟单个数据
-            Student student = new Student()
-            {
-                Id = 1,
-                Name = "李四",
-                Pwd = "1231456"
-            };
-            //_redis.KeyExpire("list",DateTime.Now.AddSeconds(20));//设置过期时间,以秒为单位
-            //_redis.KeyDelete("name");//删除缓存
+        ////redis key
+        //int REDISKEY = 1;
+        ////得到Redis缓存数据
+        //[HttpGet]
+        //public string GetRedis(EnumType.RedisType redisType)
+        //{
+        //    //集合模拟数据
+        //    List<Student> students = new List<Student>()
+        //    {
+        //        new Student { Id = 1, Name = "王", Pwd = "111" },
+        //        new Student { Id = 2, Name = "王12", Pwd = "222" },
+        //        new Student { Id = 3, Name = "王23", Pwd = "333" }
+        //    };
+        //    //模拟单个数据
+        //    Student student = new Student()
+        //    {
+        //        Id = 1,
+        //        Name = "李四",
+        //        Pwd = "1231456"
+        //    };
+        //    //_redis.KeyExpire("list",DateTime.Now.AddSeconds(20));//设置过期时间,以秒为单位
+        //    //_redis.KeyDelete("name");//删除缓存
 
-            //将RedisKey加一
-            REDISKEY += 1;
-            //添加Redis缓存
-            AddRedis(redisType, REDISKEY.ToString(), JsonConvert.SerializeObject(students));
+        //    //将RedisKey加一
+        //    REDISKEY += 1;
+        //    //添加Redis缓存
+        //    AddRedis(redisType, REDISKEY.ToString(), JsonConvert.SerializeObject(students));
 
-            //添加Redis缓存
-            AddRedis(redisType = (EnumType.RedisType)4, student.Id.ToString(), JsonConvert.SerializeObject(student));
+        //    //添加Redis缓存
+        //    AddRedis(redisType = (EnumType.RedisType)4, student.Id.ToString(), JsonConvert.SerializeObject(student));
 
 
-            //获取枚举类型对应的值
-            //string redisKey = Enum.GetName(typeof(EnumType.RedisType), redisType);
-            string redisKey = redisType.ToString();
-            //拼接Redis的Key名
-            redisKey += ("_" + student.Id);
+        //    //获取枚举类型对应的值
+        //    //string redisKey = Enum.GetName(typeof(EnumType.RedisType), redisType);
+        //    string redisKey = redisType.ToString();
+        //    //拼接Redis的Key名
+        //    redisKey += ("_" + student.Id);
 
-            //判断该key是否存在
-            //_redis.CacheRedis.KeyExists(redisKey);
+        //    //判断该key是否存在
+        //    //_redis.CacheRedis.KeyExists(redisKey);
 
-            //返回key所存储的数据类型
-            //var redisKeyType = _redis.CacheRedis.KeyType(redisKey);
+        //    //返回key所存储的数据类型
+        //    //var redisKeyType = _redis.CacheRedis.KeyType(redisKey);
 
-            //根据RedisKey读取Redis缓存数据
-            var studentRedis = _redis.CacheRedis.StringGet(redisKey);
+        //    //根据RedisKey读取Redis缓存数据
+        //    var studentRedis = _redis.CacheRedis.StringGet(redisKey);
 
-            //判断是否从Redis中取到数据
-            if (studentRedis == "")
-            {
-                //Redis没有，在数据库中查找
-            }
-            try
-            {
-                //转换为集合
-                List<Student> listStudent = JsonConvert.DeserializeObject<List<Student>>(studentRedis);
-                return JsonConvert.SerializeObject(listStudent);
+        //    //判断是否从Redis中取到数据
+        //    if (studentRedis == "")
+        //    {
+        //        //Redis没有，在数据库中查找
+        //    }
+        //    try
+        //    {
+        //        //转换为集合
+        //        List<Student> listStudent = JsonConvert.DeserializeObject<List<Student>>(studentRedis);
+        //        return JsonConvert.SerializeObject(listStudent);
 
-            }
-            catch (Exception)
-            {
-                //转换为对象
-                Student singleStudent = JsonConvert.DeserializeObject<Student>(studentRedis);
-                return JsonConvert.SerializeObject(singleStudent);
+        //    }
+        //    catch (Exception)
+        //    {
+        //        //转换为对象
+        //        Student singleStudent = JsonConvert.DeserializeObject<Student>(studentRedis);
+        //        return JsonConvert.SerializeObject(singleStudent);
 
-            }
+        //    }
 
-        }
+        //}
 
-        public void AddRedis(EnumType.RedisType redisType, string key, string value)
-        {
-            //获取枚举类型对应的值
-            //string redisKey = Enum.GetName(typeof(RedisType), redisType);
-            string redisKey = redisType.ToString();
-            //拼接Redis的Key名
-            redisKey += ("_" + key);
+        //public void AddRedis(EnumType.RedisType redisType, string key, string value)
+        //{
+        //    //获取枚举类型对应的值
+        //    //string redisKey = Enum.GetName(typeof(RedisType), redisType);
+        //    string redisKey = redisType.ToString();
+        //    //拼接Redis的Key名
+        //    redisKey += ("_" + key);
 
-            //写入Redis缓存
-            _redis.CacheRedis.StringSet(redisKey, value);
+        //    //写入Redis缓存
+        //    _redis.CacheRedis.StringSet(redisKey, value);
 
-        }
+        //}
 
 
 
